6b9304f9b61843719bb6595c08beb76e5bca8c55
seregaklim <seregaklim@mail.ru>
2021-07-13 17:12:16 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun  main () {
+     var likes = 5
+     if (likes ==1){ println("Вам поставили $likes лайк")} else  if (likes >=2 && likes <= 4)
+     {println("Вам поставили $likes лайка")} else if (likes >=4 && likes <= 21) { println("Вам поставили $likes лайков")}
+}
+
+
+
+
+
